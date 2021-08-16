b2fe79cfb9d84bbd0e1035e8f020075ecdd649cd
Kevin Gorham <kevin.gorham@electriccoin.co>
2021-08-16 14:25:59 -0400

    Add simple basic one-screen app.
    Generated by template. Nothing interesting here.

---
+++ samples/uniffikation/app/src/main/java/cash/z/ecc/android/uniffikation/MainActivity.kt
@@ -0,0 +1,11 @@
+package cash.z.ecc.android.uniffikation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
